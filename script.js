05e2b2cce44d8ad915e9766a92bbfcac359da78c
JuanMG22 <88951217+JuanMG22@users.noreply.github.com>
2021-10-17 17:06:03 -0300

    Incorporar Arrays

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
-// Array de personas que contiene datos de nombre y edad
-const personas = [{
-        nombre: `José`,
-        edad: 20,
-    },
-    {
-        nombre: `Juan`,
-        edad: 18,
-    },
-    {
-        nombre: `Julia`,
-        edad: 38,
-    },
-    {
-        nombre: `Maria`,
-        edad: 55,
-    },
-    {
-        nombre: `Pedro`,
-        edad: 23,
-    },
-    {
-        nombre: `Natalia`,
-        edad: 20,
-    },
-];
+// Creo una clase con el nombre Productos
+class Productos {
+    constructor(nombre, codigo, precio) {
+        this.nombre = nombre;
+        this.codigo = parseInt(codigo);
+        this.precio = parseFloat(precio);
+    }
 
-// Función general que inicie el proceso
-function ordenarDatos() {
-    let ordenar = prompt(`Ingrese "si" para ordenar datos de personas por edad\nEn caso contrario ingrese cualquier otra cosa`);
+}
 
-    if (ordenar == `si`) {
-        // Se ordena array con metodo sort y se ordena por edad, de menor a mayor
-        personas.sort((a, b) => {
-            if (a.edad > b.edad) {
-                return 1;
-            }
+const todosLosProductos = []; //creo un array vacío, donde despues agrego los productos.
 
-            if (a.edad < b.edad) {
-                return -1;
-            }
 
-            // En caso de que se posea la misma edad, se empieza a ordenar por nombre
-            if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
-                return 1;
-            }
+// Creo función que inicia el proceso de carga de productos
+function iniciar() {
+    let nProductos = parseInt(prompt("Ingresa la cantidad de productos que quiere cargar"));
 
-            if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
-                return -1;
-            }
-        })
+    if (nProductos > 0) {
+        for (let i = 1; i <= nProductos; i++) {
 
-        console.log(personas);
+            let nombreProducto = prompt("Ingrese nombre de producto");
+            let codigoProducto = prompt("Ingrese codigo de producto");
+            let precioProducto = prompt("Ingrese precio de producto");
 
+            const newProducto = new Productos(nombreProducto, codigoProducto, precioProducto);
+            todosLosProductos.push(newProducto); //Agrego el producto nuevo al array de productos.
+
+            console.log(`Nombre de producto: ${newProducto.nombre}\nCodigo de Producto: ${newProducto.codigo}\nPrecio de Producto: $${newProducto.precio}`);
+        }
+
+    } else {
+        alert("No ingresó un número válido")
     }
 }
 
 
-console.log(personas);
+// Función para modificar producto
+function modificarProducto() {
+    //Pido que ingrese el producto que quiere modificar
+    const nombre = prompt("Ingresa el nombre de producto que quiere modificar")
+    //Encuentro la posición dentro del array de productos
+    const indiceProducto = todosLosProductos.findIndex(e => e.nombre == nombre);
+    //Pregunto qué propiedad quiere modificar
+    const propiedadModificar = prompt("Ingresa La propiedad de producto que quiere modificar").toLowerCase();
+    //Según la propiedad del producto, modifico
+    if (propiedadModificar == "nombre") {
+        //Uso el indice para modificarlo directamente del array
+        todosLosProductos[indiceProducto].nombre = prompt("Ingrese nuevo nombre");
+        //Muestro el producto modificado
+        console.log("Producto modificado", todosLosProductos[indiceProducto]);
+
+    } else if (propiedadModificar == "codigo") {
+        todosLosProductos[indiceProducto].codigo = prompt("Ingrese nuevo codigo");
+        console.log("Producto modificado", todosLosProductos[indiceProducto]);
+
+    } else if (propiedadModificar == "precio") {
+        todosLosProductos[indiceProducto].precio = prompt("Ingrese nuevo precio");
+        console.log("Producto modificado", todosLosProductos[indiceProducto]);
+
+    } else {
+        alert("No eligio una propiedad disponible, intente nuevamente");
+    }
+
+    console.log("lista de productos actualizada", todosLosProductos); //Muestro actualización
+}
